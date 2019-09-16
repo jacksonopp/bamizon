93484ab2445d43484393f431bc2e00235ac622a4
Jackson Oppenheim <jacksonopp@gmail.com>
2019-09-16 15:51:14 -0500

    added bamizon.js, updated init.sql

--- sql/bamizon.sql
+++ sql/bamizon.sql
@@ -13,3 +13,21 @@ CREATE TABLE products (
 );
 
 SELECT * FROM products;
+
+insert into products(product_name, department_name, price, stock_qty)
+values ("motherboard", "internals", 100.00, 100);
+
+insert into products(product_name, department_name, price, stock_qty)
+values ("graphics card", "graphics", 100.00, 100);
+
+insert into products(product_name, department_name, price, stock_qty)
+values ("audio card", "audio", 100.00, 100);
+
+insert into products(product_name, department_name, price, stock_qty)
+values ("case", "externals", 100.00, 100);
+
+insert into products(product_name, department_name, price, stock_qty)
+values ("psu", "power", 100.00, 100);
+
+insert into products(product_name, department_name, price, stock_qty)
+values ("monitor", "externals", 100.00, 100);
